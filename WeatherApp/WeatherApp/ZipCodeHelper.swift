708fbe2d6d41dd6d422a43f15ca1a87b82a8abbb
Ben Stone <benjamin.h.stone@gmail.com>
2019-10-10 15:32:25 -0400

    Update ZipCodeHelper.swift

--- WeatherApp/WeatherApp/ZipCodeHelper.swift
+++ WeatherApp/WeatherApp/ZipCodeHelper.swift
@@ -1,31 +1,40 @@
-//
-//  ZipCodeHelper.swift
-//  WeatherApp
-//
-//  Created by Alex Paul on 1/17/19.
-//  Copyright © 2019 Pursuit. All rights reserved.
-//
-//  Returns the locality name for a given zip code
-//  e.g enter 10023 zip code will return New York
-//  See WeatherAppTests
-
-import Foundation
+import UIKit
 import CoreLocation
 
+enum LocationFetchingError: Error {
+    case error(Error)
+    case noErrorMessage
+}
+
 class ZipCodeHelper {
-  private init() {}
-  static func getLocationName(from zipCode: String, completionHandler: @escaping (Error?, String?) -> Void) {
-    let geocoder = CLGeocoder()
-    DispatchQueue.global(qos: .userInitiated).async {
-      geocoder.geocodeAddressString(zipCode){(placemarks, error) -> Void in
-        DispatchQueue.main.async {
-          if let placemark = placemarks?.first, let name = placemark.locality {
-            completionHandler(nil, name)
-          } else {
-            completionHandler(error, nil)
-          }
+    private init() {}
+    static func getLatLong(fromZipCode zipCode: String, completionHandler: @escaping (Result<(lat: Double, long: Double), Error>) -> Void) {
+        let geocoder = CLGeocoder()
+        DispatchQueue.global(qos: .userInitiated).async {
+            geocoder.geocodeAddressString(zipCode){(placemarks, error) -> Void in
+                DispatchQueue.main.async {
+                    if let placemark = placemarks?.first, let coordinate = placemark.location?.coordinate {
+                        completionHandler(.success((coordinate.latitude, coordinate.longitude)))
+                    } else {
+                        let locationError: LocationFetchingError
+                        if let error = error {
+                            locationError = .error(error)
+                        } else {
+                            locationError = .noErrorMessage
+                        }
+                        completionHandler(.failure(locationError))
+                    }
+                }
+            }
         }
-      }
     }
-  }
+}
+
+ZipCodeHelper.getLatLong(fromZipCode: "60613") { (result) in
+    switch result {
+    case let .success((lat, long)):
+        print(lat, long)
+    case let .failure(error):
+        print(error)
+    }
 }
